0fc01e473e4f3b66d890cbeff7bc8762adf3d6a0
Shunde Zhang <shunde@aptira.com>
2017-10-24 17:52:37 +1100

    stress test

---
+++ tests/monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+KEY_FILE=$1
+MONITOR_HOST=$2
+
+while true
+do
+    ssh -i $KEY_FILE heat-admin@$MONITOR_HOST "sudo mysql -u root -e \"SHOW GLOBAL STATUS LIKE 'threads_connected';\" -N -B" 2>/dev/null
+    ssh -i $KEY_FILE heat-admin@$MONITOR_HOST "sudo mysql -u root -e \"SHOW GLOBAL STATUS LIKE 'Threads_running';\" -N -B" 2>/dev/null
+    ssh -i $KEY_FILE heat-admin@$MONITOR_HOST "sudo rabbitmqctl -t 1 list_queues name consumers consumer_utilisation memory 2>/dev/null| grep conductor|grep -v fanout"
+
+    sleep 5
+done
