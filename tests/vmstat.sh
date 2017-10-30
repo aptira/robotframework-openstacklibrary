3faf77156ec1612966f23d47dfed0c67bbf3f91b
Shunde Zhang <shunde@aptira.com>
2017-10-30 12:05:34 +1100

    bug fix

--- tests/vmstat.sh
+++ tests/vmstat.sh
@@ -4,4 +4,10 @@ KEY_FILE=$1
 MONITOR_HOST=$2
 LOG_FILE=$3
 
-ssh -i $KEY_FILE cloud-user@$MONITOR_HOST vmstat 2 > $LOG_FILE
+ssh -i $KEY_FILE cloud-user@$MONITOR_HOST vmstat 1 1 2>/dev/null
+sleep 1
+while true
+do
+    ssh -i $KEY_FILE cloud-user@$MONITOR_HOST vmstat 1 1 2>/dev/null | tail -n1
+    sleep 1
+done
