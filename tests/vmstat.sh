e3bea42e5ed2df505d9c62f79a8172fc110b7d56
Shunde Zhang <shunde@aptira.com>
2017-10-30 11:21:48 +1100

    bug fix

--- tests/vmstat.sh
+++ tests/vmstat.sh
@@ -2,5 +2,6 @@
 
 KEY_FILE=$1
 MONITOR_HOST=$2
+LOG_FILE=$3
 
-ssh -i $KEY_FILE cloud-user@$MONITOR_HOST "sudo vmstat 2" 2>/dev/null
+ssh -i $KEY_FILE cloud-user@$MONITOR_HOST "sudo vmstat 2" 2>/dev/null 1>$LOG_FILE
